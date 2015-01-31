9b40ae459dab370169ac37b518df1c7a89a3cf45
WhiplashD <toughtanx@hotmail.com>
2015-01-30 23:20:23 -0800

    Removed old generateBetweenTiles method

--- core/src/com/fuzzy/autocity/generators/builders/PathBuilder.java
+++ core/src/com/fuzzy/autocity/generators/builders/PathBuilder.java
@@ -18,42 +18,6 @@ public class PathBuilder implements Invokable {
         this.world = world;
     }
 
-    public void generateBetweenTiles(Tile tile1, Tile tile2) throws TileOutOfBoundsException {
-        int xDiff = tile1.getX() - tile2.getX();
-        int yDiff = tile1.getY() - tile2.getY();
-
-        System.out.println("Generating between " + tile1.getX() + "," + tile1.getY() + " and " + tile2.getX() + "," + tile2.getY());
-
-        if (xDiff == 0 && yDiff == 0) {
-            System.out.println("Road complete.");
-            //no further generation required
-        } else if (Math.abs(xDiff) > Math.abs(yDiff)) {
-            System.out.println("Going across x axis.");
-
-            for (int x = tile1.getX(); x <= tile1.getX() + Math.abs(xDiff); x++) {
-                if (isInvalidTile(this.world.getTile(x, tile1.getY()))) {
-                    System.out.println("Placing road at " + x + "," + tile1.getY());
-                    Road r = new Road();
-                    this.world.buildConstruction(r, this.world.getTile(x, tile1.getY()));
-                }
-            }
-
-            this.generateBetweenTiles(this.world.getTile(tile2.getX(), tile1.getY()), tile2);
-        } else {
-            System.out.println("Going across y axis.");
-
-            for (int y = tile1.getY(); y <= tile1.getY() + Math.abs(yDiff); y++) {
-                if (isInvalidTile(this.world.getTile(tile1.getX(), y))) {
-                    System.out.println("Placing road at " + tile1.getX() + "," + y);
-                    Road r = new Road();
-                    this.world.buildConstruction(r, this.world.getTile(tile1.getX(), y));
-                }
-            }
-
-            this.generateBetweenTiles(this.world.getTile(tile1.getX(), tile2.getY()), tile2);
-        }
-    }
-
     public void generate(Tile startTile, Tile targetTile, int maxSearchDistance) {
         aStarPathFinder f = new aStarPathFinder(this.world, maxSearchDistance);
         try {
